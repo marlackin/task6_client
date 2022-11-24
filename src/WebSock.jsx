309771dbb3bcef3ccd40897690541e030b8ef18f
Sasha Soikel <mazurova1813@mail.ru>
2022-11-25 02:31:22 +0300

    last changes

--- src/WebSock.jsx
+++ src/WebSock.jsx
@@ -1,7 +1,9 @@
 import React, {useEffect, useRef, useState} from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
 import {Form,Container,Button,Alert,ListGroup,Nav} from 'react-bootstrap';
 import axios from 'axios';
 import { Navigate, useNavigate } from 'react-router-dom';
+import DropDown from './DropDown'
 
 const WebSock= ()=> {
     const [messages, setMessages] = useState([]);
@@ -9,9 +11,9 @@ const WebSock= ()=> {
     const socket = useRef('')
     const [connected, setConnected] = useState(false);
     const [username, setUsername] = useState({userName:''})
-    const [themes, setTheme] = useState([])
     const [users,setUsers] = useState([]);
-    const [usersOptions, setUsersOptions] = useState({userName:'',theme:''})
+    const [usersOptions, setUsersOptions] = useState({userTo:'',theme:'',message:''})
+    const [userMessages, setUserMessages] = useState([])
 
 
      const connect = ()=> {
@@ -20,15 +22,29 @@ const WebSock= ()=> {
         socket.current.onopen = () => {
             setConnected(true)
             const user = {
-                event: 'connection',
-                username,
-                id: Date.now()
+              userName:username.userName,
+              theme:usersOptions.theme,
+              userTo:usersOptions.userTo,
+              message: usersOptions.message,
+              date: usersOptions.date,
+              event: 'connection'
             }
-            socket.current.send(JSON.stringify(user))
+              axios.post('http://localhost:5000/allUsersMessage',{userName:username.userName})
+          .then(res=>{
+             if(res.status === 200){
+              console.log(res)
+              setUsers(res.data)
+              setUserMessages(res.data);
+              console.log(res.data)
+            }
+          }) 
+          .catch(console.log("fasfasf"))
+          socket.current.send(JSON.stringify(user))
         }
         socket.current.onmessage = (event) => {
             const message = JSON.parse(event.data)
-            setMessages(prev => [message, ...prev])
+            console.log(message)
+            setUserMessages(prev => [message, ...prev])
         }
         socket.current.onclose= () => {
             console.log('Socket закрыт')
@@ -38,16 +54,7 @@ const WebSock= ()=> {
         }
     }
 
-    // useEffect(() =>{
-    //     axios.get('http://localhost:5000/getAllUsers',username.userName)
-    //     .then(res=>{
-    //        if(res.status === 200){
-    //         setUsers(res.data);
-    //         console.log(users)
-    //       }
-    //     }) 
-    //     .catch(console.log("fasfasf"))
-    //  },[])
+
 
     const handleSubmit = (event) => {
       event.preventDefault()
@@ -55,7 +62,7 @@ const WebSock= ()=> {
       axios.post('http://localhost:5000/register',username)
       .then(res=>{
          if(res.status === 200){
-          alert('вы выошли')
+          alert('вы вошли')
         }
       })
       .catch(err=>{if(err.response){
@@ -77,6 +84,9 @@ const WebSock= ()=> {
     )
   }
 
+
+
+
   const usersOption = () =>{
             axios.post('http://localhost:5000/getAllUsers',{userName:username.userName})
         .then(res=>{
@@ -88,54 +98,76 @@ const WebSock= ()=> {
         }) 
         .catch(console.log("fasfasf"))
   }
-  const currentUser=(currentUser)=>{
-    console.log(currentUser)
-  }
 
 
-  const sendMessage = async () => {
-    const message = {
-        username:username.userName,
-        message: value,
-        id: Date.now(),
-        event: 'message'
+  const sendMessage = (event) =>{
+    event.preventDefault()
+    const messagesUser = {
+      userName:username.userName,
+      theme:usersOptions.theme,
+      userTo:usersOptions.userTo,
+      message: usersOptions.message,
+      date:new Date().toLocaleString(),
+      event: 'message'
     }
-    socket.current.send(JSON.stringify(message));
-    setValue('')
+    axios.post('http://localhost:5000/sendMessage',messagesUser)
+.then(res=>{
+   if(res.status === 200){
+    console.log(res)
+    socket.current.send(JSON.stringify(messagesUser));
+    console.log('я тут')
+    // setMessages(prev => [messages, ...prev])
+    // setUsers(res.data);
+    // console.log(users)
+  }
+}) 
+.catch(console.log("fasfasf"))
 }
 
+//   const sendMessage = async () => {
+//     const message = {
+//         username:username.userName,
+//         theme:usersOptions.theme,
+//         userTo:usersOptions.userTo,
+//         message: usersOptions.message,
+//         date: new Date().toLocaleString(),
+//         event: 'message'
+//     }
+//     console.log(message)
+//     socket.current.send(JSON.stringify(message));
+//     setValue('')
+// }
 
   return (
     <>
     <Container className='container-my'>
-    <Form>
-    <Form.Select onClick={usersOption} onChange={e=>setUsersOptions(e.target.value)} aria-label="Default select example">
+    <Form onSubmit={sendMessage}>
+    <Form.Select onClick={usersOption} onChange={e=>setUsersOptions({...usersOptions,userTo: e.target.value})} aria-label="Default select example">
+      <option disabled>Выберите собеседника</option>
       {users.map(user =><option  key={user.userName}>
         {user.event === 'connection' 
-        ? user.username.userName
+        ? user.userName
         : user.userName
         }
         </option>)}
     </Form.Select>
-      {/* {users.map(user =><Form.Select  key={user.userName}>
-        {user.event === 'connection' 
-        ? <option className='alert'>пользователь {user.username.userName} подключился</option>
-        : <option className='alert'>{user.userName}</option>
-        }
-        </Form.Select>)} */}
+    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Тема</Form.Label>
+        <Form.Control onChange={e=>setUsersOptions({...usersOptions,theme: e.target.value})} type="text" placeholder="Введите тему" />
+      </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         <Form.Label  className='label'>Введите сообщение</Form.Label>
-        <Form.Control value={value} onChange={e=>setValue(e.target.value)} as="textarea" rows={2} />
-        <Button onClick={sendMessage} variant="outline-success" className='btn-send'>Отправить</Button>
+        <Form.Control onChange={e=>setUsersOptions({...usersOptions,message: e.target.value})} as="textarea" rows={2} />
       </Form.Group>
+      <Button type="submit" variant="outline-success" className='btn-send'>Отправить</Button>
     </Form>        
     </Container>
      <Container >
     <Form.Label>
-        {messages.map(mess =><Form.Label className='label' key={mess.id}>
-        {mess.event === 'connection' 
-        ? <Alert className='alert'>пользователь {mess.username.userName} подключился</Alert>
-        : <Alert className='alert'>{mess.username}: {mess.message} {new Date().toLocaleString()}</Alert>
+        {userMessages && userMessages.map(user =><Form.Label className='label alert_mess' key={user._id}>
+        {user.event === 'connection' 
+        ? <Alert className='alert'>пользователь {user.userName} подключился</Alert>
+        : <Alert className='alert'>От: {user.userName} | Кому: {user.userTo} | Тема: {user.theme} | Сообщение :{user.message} | Дата {user.date}</Alert>
         }
         </Form.Label>)}
         </Form.Label>
