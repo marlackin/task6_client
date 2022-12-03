9035aaa41bc0e6336d418a30073f3a98508b4168
Sasha Soikel <mazurova1813@mail.ru>
2022-12-03 03:24:46 +0300

    deployws

--- src/WebSock.jsx
+++ src/WebSock.jsx
@@ -17,7 +17,8 @@ const WebSock= ()=> {
 
 
      const connect = ()=> {
-        socket.current = new WebSocket("ws://task6serverwebsocket-production.up.railway.app/:7000",)
+        socket.current = new WebSocket("wss://task6serverwebsocket-production.up.railway.app",)
+        // socket.current = new WebSocket("ws://localhost:7000",)
 
         socket.current.onopen = () => {
             setConnected(true)
